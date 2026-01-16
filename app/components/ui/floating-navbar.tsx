e51068e84708ee0f1162802cf81839e3b678a6f4
belovedtoluwanimi <belovedtoluwanimi16@gmail.com>
2026-01-16 08:54:35 +0100

    Initial commit: Next.js project setup

--- app/components/ui/floating-navbar.tsx
+++ app/components/ui/floating-navbar.tsx
@@ -53,7 +53,7 @@ export const FloatingNav = ({
         duration: 0.2,
       }}
       className={cn(
-        "fixed top-10 inset-x-0 mx-auto border border-transparent dark:border-green-500/50 rounded-full dark:bg-black/50 bg-white shadow-lg z-[5000] pr-2 pl-8 py-2 flex items-center justify-center space-x-4 max-w-fit",
+        "fixed top-10 inset-x-0 mx-auto border border-transparent dark:border-green-500/50 rounded-full bg-black/50 shadow-lg z-[5000] pr-2 pl-8 py-2 flex items-center justify-center space-x-4 max-w-fit",
         className
       )}
     >
@@ -62,7 +62,7 @@ export const FloatingNav = ({
           key={`link=${idx}`}
           href={navItem.link}
           className={cn(
-            "relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-green-300 hover:text-green-500"
+            "relative text-neutral-50 items-center flex space-x-1 hover:text-green-300 "
           )}
         >
           <span className="block sm:hidden">{navItem.icon}</span>
@@ -70,7 +70,7 @@ export const FloatingNav = ({
         </a>
       ))}
       <Link href="/auth">
-      <button className="border text-sm cursor-pointer font-medium relative border-neutral-200 dark:border-green-500/50 text-black dark:text-white px-4 py-2 rounded-full">
+      <button className="border text-sm cursor-pointer font-medium relative border-green-500/50  text-white px-4 py-2 rounded-full">
         <span>Login</span>
         <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-green-200 to-transparent h-px" />
       </button>
